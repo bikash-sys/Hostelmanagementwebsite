708c49360598e651cbd914f9c8aecd36b6c51dd5
Bikash <bikjha2007@gmail.com>
2026-05-17 23:04:43 +0530

    mail

--- src/app/components/BookingForm.jsx
+++ src/app/components/BookingForm.jsx
@@ -50,7 +50,7 @@ export function BookingForm({ selectedRoom, onBack }) {
 
     // NOTE: Replace these with your actual EmailJS IDs and Keys!
     emailjs.send(
-      'service_x02nmf9',
+      'service_8yeyrwd',
       'template_6w6ox5i',
       templateParams,
       'H2wCMESMkzRAYi5A8'
